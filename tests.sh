d53ec1e50e313d9bc3ee9584f1fc0c9d87e6cbb5
Marius Meisenzahl <mariusmeisenzahl@gmail.com>
2021-04-06 19:49:15 +0200

    Lint wallpapers with command line tool

--- tests.sh
+++ tests.sh
@@ -1,22 +1,9 @@
 #!/bin/sh
 set -e
 
-# Ensure Artist metadata is not corrupt, does exist and at least 3 chars
-test_artist () {
-    print_error () {
-        echo "Failure! $1 missing Artist metadata! See README.md."
-        exit 1
-    }
-
-    ARTIST="$(exif --tag=Artist --no-fixup -m "$1")" || print_error "$1"
-    if [ "$(echo "$ARTIST" | wc -m)" -lt 3 ]; then
-        print_error "$1"
-    fi
-}
-
 for WALLPAPER in backgrounds/*.jpg
 do
-    test_artist "$WALLPAPER"
+    ./build/src/io.elementary.wallpapers --lint "$WALLPAPER"
 done
 
-echo "Success! Wallpapers have Artist metadata."
+echo "Success! Wallpapers have valid metadata."
